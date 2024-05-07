6489383d5812a9d26b251a14eb5f52e51a19b303
mwagnerretro <148266620+mwagnerretro@users.noreply.github.com>
2024-05-07 10:30:10 -0400

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,59 @@
+#include <SFML/Graphics.hpp>
+#include <time.h>
+using namespace sf;
+
+const int M = 20;
+const int N = 10;
+
+int field[M][N] = { 0 };
+
+struct Point
+{
+	int x, y;
+} a[4], b[4];
+
+int figures[7][4] =
+{
+	1,3,5,7, // I
+	2,4,5,7, // Z
+	3,5,4,6, // S
+	3,5,4,7, // T
+	2,3,5,7, // L
+	3,5,7,6, // J
+	2,3,4,5, // O
+};
+
+bool check()
+{
+	for (int i = 0; i < 4; i++)
+		if (a[i].x < 0 || a[i].x >= N || a[i].y >= M) return 0;
+		else if (field[a[i].y][a[i].x]) return 0;
+
+	return 1;
+};
+
+int main()
+{
+	RenderWindow window(VideoMode(320, 480), "Tetris!!");
+
+	Texture t1;
+	t1.loadFromFile("Textures/tiles.png");
+	Sprite s(t1);
+
+	while (window.isOpen())
+	{
+		Event e;
+		while (window.pollEvent(e))
+		{
+			if (e.type == Event::Closed)
+				window.close();
+
+		}
+
+		window.clear(Color::White);
+		window.display();
+		window.draw(s);
+	}
+
+	return 0;
+}
