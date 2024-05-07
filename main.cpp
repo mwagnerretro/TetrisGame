666eb353cbaba37a18902091ef773dc77f1a46f9
mwagnerretro <148266620+mwagnerretro@users.noreply.github.com>
2024-05-07 10:31:09 -0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -29,30 +29,128 @@ bool check()
 		if (a[i].x < 0 || a[i].x >= N || a[i].y >= M) return 0;
 		else if (field[a[i].y][a[i].x]) return 0;
 
-	return 1;
+		return 1;
 };
+
 
 int main()
 {
+	srand(time(0));
+
 	RenderWindow window(VideoMode(320, 480), "Tetris!!");
 
-	Texture t1;
+	Texture t1, t2, t3;
 	t1.loadFromFile("Textures/tiles.png");
-	Sprite s(t1);
+	t2.loadFromFile("Textures/background.png");
+	t3.loadFromFile("Textures/frame.png");
+
+	Sprite s(t1), background(t2), frame(t3);
+
+	int dx = 0; 
+	bool rotate = 0; 
+	int colorNum = 1;
+	float timer = 0, delay = 0.3;
+
+	Clock clock;
 
 	while (window.isOpen())
 	{
+		float time = clock.getElapsedTime().asSeconds();
+		clock.restart();
+		timer += time;
+
 		Event e;
 		while (window.pollEvent(e))
 		{
 			if (e.type == Event::Closed)
 				window.close();
 
+			if (e.type == Event::KeyPressed)
+				if (e.key.code == Keyboard::Up) rotate = true;
+				else if (e.key.code == Keyboard::Left) dx = -1;
+				else if (e.key.code == Keyboard::Right) dx = 1;
 		}
 
+		if (Keyboard::isKeyPressed(Keyboard::Down)) delay = 0.05;
+
+		//Movement Code
+		for (int i = 0; i < 4; i++) { b[i] = a[i]; a[i].x += dx; }
+		if (!check()) for (int i = 0; i < 4; i++) a[i] = b[i];
+
+		//Rotation
+		if (rotate)
+		{
+			Point p = a[1]; //center of rotation
+			for (int i = 0; i < 4; i++)
+			{
+				int x = a[i].y - p.y;
+				int y = a[i].x - p.x;
+				a[i].x = p.x - x;
+				a[i].y = p.y + y;
+			}
+			if (!check()) for (int i = 0; i < 4; i++) a[i] = b[i];
+		}
+
+		//Timer
+		if (timer > delay)
+		{
+			for (int i = 0; i < 4; i++) { b[i] = a[i]; a[i].y += 1; }
+
+			if (!check())
+			{
+				for (int i = 0; i < 4; i++) field[b[i].y][b[i].x] = colorNum;
+
+				colorNum = 1 + rand() % 7;
+				int n = rand() % 7;
+				for (int i = 0; i < 4; i++)
+				{
+					a[i].x = figures[n][i] % 2;
+					a[i].y = figures[n][i] / 2;
+				}
+			}
+
+			timer = 0;
+		}
+
+		//check lines
+		int k = M - 1;
+		for (int i = M - 1; i > 0; i--)
+		{
+			int count = 0;
+			for (int j = 0; j < N; j++)
+			{
+				if (field[i][j]) count++;
+				field[k][j] = field[i][j];
+			}
+			if (count < N) k--;
+		}
+
+		dx = 0; rotate = 0; delay = 0.3;
+
+		//drawing
 		window.clear(Color::White);
+		window.draw(background);
+
+		for (int i = 0; i < M; i++)
+			for (int j = 0; j < N; j++)
+			{
+				if (field[i][j] == 0) continue;
+				s.setTextureRect(IntRect(field[i][j] * 18, 0, 18, 18));
+				s.setPosition(j * 18, i * 18);
+				s.move(28, 31);
+				window.draw(s);
+			}
+
+		for (int i = 0; i < 4; i++)
+		{
+			s.setTextureRect(IntRect(colorNum * 18, 0, 18, 18));
+			s.setPosition(a[i].x * 18, a[i].y * 18);
+			s.move(28, 31);
+			window.draw(s);
+		}
+
+		window.draw(frame);
 		window.display();
-		window.draw(s);
 	}
 
 	return 0;
